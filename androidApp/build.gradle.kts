4f56ff29ca5a4009f912aff27ffccdf38ec035d1
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2026-06-08 00:45:21 +0200

    Add Koin Android dependency
    Add Koin Android to project catalog and include it in the Android app module. Updates gradle/libs.versions.toml to register io.insert-koin:koin-android (version ref 'koin') and adds implementation(libs.koin.android) to androidApp/build.gradle.kts to enable Koin DI in the app.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -19,6 +19,9 @@ dependencies {
 
     implementation(libs.compose.uiToolingPreview)
     debugImplementation(libs.compose.uiTooling)
+
+    // Added
+    implementation(libs.koin.android)
 }
 
 android {
