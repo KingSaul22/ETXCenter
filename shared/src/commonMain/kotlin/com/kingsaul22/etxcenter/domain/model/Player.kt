ef43e86e257abd2d97904790ba13c01f125838cc
KingSaul22 <saul.patrocinio.garcia22@gmail.com>
2026-06-07 20:03:12 +0200

    Add Player and Team domain models
    Introduce two Kotlin data classes in com.kingsaul22.etxcenter.domain.model: Player (id, displayName, nullable teamId) and Team (id, name). These simple domain entities provide core representations for players and teams used throughout the app.

---
+++ shared/src/commonMain/kotlin/com/kingsaul22/etxcenter/domain/model/Player.kt
@@ -0,0 +1,7 @@
+package com.kingsaul22.etxcenter.domain.model
+
+data class Player(
+    val id: String,
+    val displayName: String,
+    val teamId: String?,
+)
---
+++ shared/src/commonMain/kotlin/com/kingsaul22/etxcenter/domain/model/Team.kt
@@ -0,0 +1,6 @@
+package com.kingsaul22.etxcenter.domain.model
+
+data class Team(
+    val id: String,
+    val name: String,
+)
